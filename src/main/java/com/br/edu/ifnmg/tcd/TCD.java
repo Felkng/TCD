79f6e1d2bae766c15dcddcbfb7f02e36235e8988
Felkng <felipersf374@gmail.com>
2023-11-25 09:25:31 -0300

    Add project

---
+++ src/main/java/com/br/edu/ifnmg/tcd/TCD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.br.edu.ifnmg.tcd;
+
+/**
+ *
+ * @author Felkng <&it;felkng374 at @gmail.com&gt;>
+ */
+public class TCD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
